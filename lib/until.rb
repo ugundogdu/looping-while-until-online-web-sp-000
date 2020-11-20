71f756c9d24c33e3ca3db336fb0f80f7438548ff
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-11-20 01:24:46 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,8 +2,8 @@ def using_until
   levitation_force = 6
   #your code here
   counter = 0
-  until counter == 10
-    puts 'Wingardium Leviosa'
+  until counter == 10 do
+    puts "Wingardium Leviosa"
     counter += 1
   end
 end
